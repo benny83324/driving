63cdb12e2df606a67d419fa7aa37935755ca6958
Aaron Ke <benny83324@gmail.com>
2019-10-20 22:43:35 -0500

    Add US

--- driving.py
+++ driving.py
@@ -1,7 +1,12 @@
-country=input("Which country were you from:")
+country=input("Which country were you from(Taiwan or US):")
 age=int(input("How old are you:"))
 if country == "Taiwan":
 	if age >= 18:
+		print("you are able to apply a license")
+	else:
+		print("you are not able to apply a license") 
+elif country == "US":
+	if age >= 16:
 		print("you are able to apply a license")
 	else:
 		print("you are not able to apply a license") 
